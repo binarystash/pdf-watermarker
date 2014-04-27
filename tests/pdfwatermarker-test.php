a55a2e3b1d45787284eb58b198e4a59dbf7bd22f
binarystash01 <binarystash01@gmail.com>
2014-04-27 13:45:04 +0800

    added tests

---
+++ tests/pdfwatermarker-test.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("../fpdf/fpdf.php");
+require_once("../fpdi/fpdi.php");
+require_once("../pdfwatermarker/pdfwatermarker.php");
+
+class PDFWatermarker_test extends PHPUnit_Framework_TestCase
+{
+    public $watermark;
+    public $watermarker;
+    public $output;
+
+    function setUp() {
+        $this->watermark = new PDFWatermark('../assets/star.png');
+
+        $this->output = tempnam( sys_get_temp_dir(), uniqid().".pdf");
+
+        $this->watermarker = new PDFWatermarker('../assets/test.pdf', $this->output, $this->watermark); 
+    }
+
+    public function testwatermarkPdf()
+    {
+        $this->watermarker->setWatermarkPosition('bottomleft');
+        $this->watermarker->watermarkPdf(); 
+        $this->assertTrue( file_exists($this->output) === true );
+    }
+}
